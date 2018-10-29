67dd78c095941fee91ae9006b16eeeae2b03e2b0
Slender1o <44403764+Slender1o@users.noreply.github.com>
2018-10-29 10:18:13 -0500

    Add files via upload
    Save

--- txt.exe.py
+++ txt.exe.py
@@ -12,6 +12,9 @@ import time
 import random
 import sys
 
+def wait(wait):
+    time.sleep(wait)
+
 def delay_print(s):
     for c in s:
         sys.stdout.write(c)
@@ -50,39 +53,105 @@ def raceI(WTR,CLTHSM,CLTHSF,RCE,INTRO):
                 print("yes")
     
 
-delay_print ("You are surrounded in darkness. Everything that you see is dark and confusing. ")
-delay_print("A warm light surrounds you and you hear a voice:\n")
-name = input("Who are you, Adventurer?\n")
-nickName = input("What is your nickname, " + name + "?\n")
-time.sleep(1)
-delay_print(name + "...that is a good, strong name.")
-delay_print(" You have a choice, " + nickName + ". You can become the fabled hero of Falte or you can die in the process.")
+print ("\n" * 64)
+
+delay_print ("You are surrounded in darkness. Everything that you see is dark and confusing.\n\n")
+
+wait(6)
+
+delay_print("A warm light surrounds you and you hear a voice:\n\n")
+
+wait(3)
+
+Name = input("Who are you, Adventurer? What is your proper name?\n\n>")
+
+nickName = input("..and your nickname, " + Name + "?\n\n>")
+
+wait(1)
+
+delay_print(name + "...that is a good, strong name. ")
+
+delay_print("You have a choice, " + nickName + ". You can become the fabled hero of Falte or you can die in the process.")
+
 delay_print(" I will now transport you into the country of Falte...\n")
+
 time.sleep(6)
+
 delay_print("You wake up in a pool of soothing water. Your memory is hazy, at best, and you can't seem to figure out where you are.")
+
 delay_print(" As you look around, you can't seem to figure out what your body looks like. You try to remember...\n")
+
 print("-------------------------------------------------------\n")
 
+
+
 delay_print("As you inspect what little you can see of your body, you remember that you are...\n")
+
 gender = input("What is your gender?\n")
+
 if (gender == 'Female'):
+
    delay_print("Right. You remember your feminine charms and whims. You are a woman.\n")
+
 if (gender == 'Male'):
+
    delay_print("Right. You remember your manly muscles and all your feats of strength. You are a man.\n")
+
+wait(6)
+
+print ("\n" * 64)
 
 raceList = [' ', 'Orc', 'Human', 'Elf', 'Dwarf', '']
 
-time.sleep(6)
-
 for i in range(1, 6):
+
     print(raceList[i])
+
     print("\n")
     
+
     
-race = input("What is your race, " + name + ". (Type in a race)\n")
+
+race = input("What is your race, " + Name + "?\n")
+
 delay_print("Yes. That's right. You're a(n) " + race + ".")
+
 time.sleep(5)
+
 print ("\n" * 64)
+
 if race == 'Elf':
+
     raceI("pool","There is a tunic and a pair of boots.","There is a dress, a hair clip and a pair of boots.",
+
           ["Elf","elf","Elven","elven"],"You look around and see that you are in a tent with guards standing outside. They are ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
